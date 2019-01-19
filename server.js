bec256bbcfac86424de871b7719070c017bb6a06
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-19 17:28:00 -0500

    Add db sync

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const routes = require('./routes');
+const db = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,14 +8,20 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
-// Start the API server
-app.listen(PORT, () => {
-  // console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// Connect to DB
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+}).catch((err) => {
+  console.error(`Error connecting to DB ===> ${err}.`);
 });
