b74b782d8f4104bec6de0cad6337b2aa76ad41b4
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-19 22:41:29 -0500

    Add get route for single record by id

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../../models');
 
+// Get all records for a given model
 router.get('/all/:model', (req, res) => {
   let model;
 
@@ -29,6 +30,45 @@ router.get('/all/:model', (req, res) => {
 
   // Query database for all records by model
   db[model].findAll({}).then((results) => {
+    res.status(200).json(results);
+  }).catch((err) => {
+    console.error(`Error returned from query ===> ${err}.`);
+  });
+});
+
+// Get single record for a given model
+router.get('/all/:model/:id', (req, res) => {
+  let model;
+
+  // Determine which model was passed from req.params
+  switch (req.params.model) {
+    case 'users':
+      model = 'User';
+      break;
+    case 'projects':
+      model = 'Project';
+      break;
+    case 'ideas':
+      model = 'Idea';
+      break;
+    case 'timesheets':
+      model = 'Timesheet';
+      break;
+    default:
+      model = '404';
+  }
+
+  // If no models match then return 404 and stop processing
+  if (model === '404') {
+    return res.status(404).end();
+  }
+
+  // Query database for all records by model
+  db[model].findAll({
+    where: {
+      id: req.params.id,
+    },
+  }).then((results) => {
     res.status(200).json(results);
   }).catch((err) => {
     console.error(`Error returned from query ===> ${err}.`);
