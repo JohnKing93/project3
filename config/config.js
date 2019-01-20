4b5ff5ace3e74833f89353503894639637b325b1
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-19 22:34:35 -0500

    Add dotenv for DB password stored in .env file

---
+++ config/config.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'stepUp_DB',
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
+
+module.exports = config;
