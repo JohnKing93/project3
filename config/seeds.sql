57dc73f19851662bb0e9dacce38b70d183f7c6c2
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-19 20:53:01 -0500

    Remove seeds.sql file

--- config/seeds.sql
+++
@@ -1,6 +0,0 @@
-USE stepup_db;
-
-INSERT INTO Status_Ref (Type, Description, createdAt, updatedAt)
-VALUES ('Project_Sts', 'In Progress', NOW(), NOW()),
-('Project_Sts', 'Completed', NOW(), NOW()),
-('Project_Sts', 'Archived', NOW(), NOW());
