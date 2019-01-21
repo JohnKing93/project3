1f678a4165755f4692f9a1f1f2e9f09cf5f162c0
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-01-21 14:59:41 -0500

    Add api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,78 +1,67 @@
 const router = require('express').Router();
 const db = require('../../models');
 
-// Get all records for a given model
-router.get('/all/:model', (req, res) => {
-  let model;
+/* Function to get all records for a given model */
+const getAllUsers = (req, res) => {
+  db.User.findAll({
+    include: [db.Permission],
+    order: ['id'],
+  })
+    .then(results => res.status(200).json(results))
+    .catch(err => console.error(`Error returned from query ===> ${err}.`));
+};
 
-  // Determine which model was passed from req.params
-  switch (req.params.model) {
-    case 'users':
-      model = 'User';
-      break;
-    case 'projects':
-      model = 'Project';
-      break;
-    case 'ideas':
-      model = 'Idea';
-      break;
-    case 'timesheets':
-      model = 'Timesheet';
-      break;
-    default:
-      model = '404';
-  }
+const getAllIdeas = (req, res) => {
+  db.Idea.findAll({
+    include: [db.User],
+    order: ['id'],
+  })
+    .then(results => res.status(200).json(results))
+    .catch(err => console.error(`Error returned from query ===> ${err}.`));
+};
 
-  // If no models match then return 404 and stop processing
-  if (model === '404') {
-    return res.status(404).end();
-  }
-
-  // Query database for all records by model
-  db[model].findAll({}).then((results) => {
-    res.status(200).json(results);
-  }).catch((err) => {
-    console.error(`Error returned from query ===> ${err}.`);
-  });
-});
-
-// Get single record for a given model
-router.get('/all/:model/:id', (req, res) => {
-  let model;
-
-  // Determine which model was passed from req.params
-  switch (req.params.model) {
-    case 'users':
-      model = 'User';
-      break;
-    case 'projects':
-      model = 'Project';
-      break;
-    case 'ideas':
-      model = 'Idea';
-      break;
-    case 'timesheets':
-      model = 'Timesheet';
-      break;
-    default:
-      model = '404';
-  }
-
-  // If no models match then return 404 and stop processing
-  if (model === '404') {
-    return res.status(404).end();
-  }
-
-  // Query database for all records by model
-  db[model].findAll({
+const getAllVotesByIdea = (req, res) => {
+  db.IdeaVote.findAll({
     where: {
-      id: req.params.id,
+      Idea_ID: parseInt(req.params.idea_id, 10),
     },
-  }).then((results) => {
-    res.status(200).json(results);
-  }).catch((err) => {
-    console.error(`Error returned from query ===> ${err}.`);
-  });
+    order: ['Idea_ID', 'id'],
+  })
+    .then(results => res.status(200).json(results))
+    .catch(err => console.error(`Error returned from query ===> ${err}.`));
+};
+
+const getAllProjects = (req, res) => {
+  db.Project.findAll({
+    include: [db.User, db.Status],
+    order: ['id'],
+  })
+    .then(results => res.status(200).json(results))
+    .catch(err => console.error(`Error returned from query ===> ${err}.`));
+};
+
+/* Function to get all records for a given model */
+
+// Route to retrieve all records for a given model
+// idea_id is optional and should only be passed when looking for all votes for an idea
+router.get('/all/:model/:idea_id?', (req, res) => {
+  switch (req.params.model) {
+    case 'users':
+      getAllUsers(req, res);
+      break;
+    // Same function for both
+    case 'ideas':
+      getAllIdeas(req, res);
+      break;
+    case 'projects':
+      getAllProjects(req, res);
+      break;
+    case 'votes':
+      getAllVotesByIdea(req, res);
+      break;
+    default:
+      res.status(404).end();
+  }
 });
 
 module.exports = router;
