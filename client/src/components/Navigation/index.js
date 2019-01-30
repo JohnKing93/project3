f5fcd532fdf3efe229a4fd59a99ac150fe6ee20f
Nadine Hernandez <nrhernandez93@gmail.com>
2019-01-29 23:02:47 -0500

    sass and module in up to date master

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export function Navigation(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark">
   <a href="/profile" className="navbar-brand">ProGro</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
       <li className="nav-item">
         <a className="nav-link" href="/profile">My Profile</a>
       </li>
