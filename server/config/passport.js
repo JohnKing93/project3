56b92713c953c2f0f53c2c7ed99adf8dc477af44
Barbara <bandezanne@gmail.com>
2019-02-04 20:07:19 -0500

    rejected changes
    trickle down from other rejections
    as per other rejections: let's avoid these types of changes

--- server/config/passport.js
+++ server/config/passport.js
@@ -15,41 +15,32 @@ passport.use(
       passwordField: 'password',
       passReqToCallback: true,
       session: false,
-    }, (req, username, password, done) => {
+       }, (req, username, password, done) => {
       try {
-        db.User
-          .findOne({
-            where: {
-              email: req.body.username,
-            },
-          // eslint-disable-next-line consistent-return
-          })
-          .then((user) => {
-            if (user !== null) {
-              return done(null, false, {
-                message: 'Email is already registered',
-              });
-            }
-            bcrypt
-              .hash(password, BCRYPT_SALT_ROUNDS)
-              .then((hashedPassword) => {
-                db.User
-                  .create({
-                    email: username,
-                    password: hashedPassword,
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    permissionID: 1,
-                  })
-                  .then((createdUser) => {
-                    console.log('User sucessfully created');
-                    return done(null, createdUser);
-                  })
-                  .catch(err => console.error(err));
-              })
-              .catch(err => console.error(err));
-          })
-          .catch(err => console.error(err));
+        db.User.findOne({
+          where: {
+            email: req.body.username,
+          },
+        // eslint-disable-next-line consistent-return
+        }).then((user) => {
+          if (user != null) {
+            return done(null, false, {
+              message: 'Email is already registered',
+            });
+          }
+          bcrypt.hash(password, BCRYPT_SALT_ROUNDS).then((hashedPassword) => {
+            db.User.create({
+              email: username,
+              password: hashedPassword,
+              firstName: req.body.firstname,
+              lastName: req.body.lastname,
+              permissionID: 1,
+            }).then((createdUser) => {
+              console.log('User sucessfully created');
+              return done(null, createdUser);
+            });
+          });
+        });
       } catch (err) {
         done(err);
       }
@@ -66,29 +57,19 @@ passport.use(
       session: false,
     }, (username, password, done) => {
       try {
-        db.User
-          .findOne({
-            where: {
-              email: username,
-            },
-          // eslint-disable-next-line consistent-return
-          })
-          .then((user) => {
-            if (user === null) {
-              return done(null, false, { message: 'User does not exist' });
-            }
-            bcrypt
-              .compare(password, user.password)
-              .then((response) => {
-                if (response !== true) {
-                  return done(null, false, { message: 'Incorrect password' });
-                }
-                console.log('Login successful');
-                return done(null, user);
-              })
-              .catch(err => console.error(err));
-          })
-          .catch(err => console.error(err));
+        db.User.findOne({
+          where: {
+            email: username,
+          },
+        // eslint-disable-next-line consistent-return
+        }).then((user) => {
+          if (user === null) {
+            return done(null, false, { message: 'User does not exist' });
+          }
+          console.log('Login successful');
+            return done(null, user);
+          });
+        });
       } catch (err) {
         done(err);
       }
@@ -103,22 +84,19 @@ passport.use(
     secretOrKey: jwtSecret.secret,
   }, (jwtPayload, done) => {
     try {
-      db.User
-        .findOne({
-          where: {
-            email: jwtPayload.username,
-          },
-        })
-        .then((user) => {
-          if (user) {
-            console.log('User found');
-            done(null, user);
-          } else {
-            console.log('User not found');
-            done(null, false);
-          }
-        })
-        .catch(err => console.error(err));
+      db.User.findOne({
+        where: {
+          email: jwtPayload.username,
+        },
+      }).then((user) => {
+        if (user) {
+          console.log('User found');
+          done(null, user);
+        } else {
+          console.log('User not found');
+          done(null, false);
+        }
+      });
     } catch (err) {
       done(err);
     }
