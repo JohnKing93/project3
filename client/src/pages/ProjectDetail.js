a185d3111b0ccd264dc4f252f816415c3703b2de
Johnny King <31927132+JohnKing93@users.noreply.github.com>
2019-02-09 11:05:51 -0500

    Merged roles and members to project detail page.

--- client/src/pages/ProjectDetail.js
+++ client/src/pages/ProjectDetail.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid";
-import { Card } from "../components/Card";
-import { Input, FormGroup, Label, Form, FormBtn } from "../components/Form";
+import { Card, ColorCard, ColorCardBody, ColorCardFooter, MemberCard, MemberCardListItem, MemberCardListGroup } from "../components/Card";
+import { Input, FormGroup, Label, Form, FormBtn, Button } from "../components/Form";
 import { RoleDropBtn, DropDown, DropDownBtn } from "../components/Buttons";
 import { List, ListItem } from "../components/List";
 import { Navigation } from "../components/Navigation";
@@ -12,6 +12,14 @@ import RolesAndMembers from "../components/RolesAndMembers";
 class ProjectDetail extends Component {
 
   state = {
+    user: {
+      id: 2,
+      permission: 1,
+    },
+    project: {
+      id: 1,
+      ownerId: 1
+    },
     title: '',
     description: '',
     owner: '',
@@ -20,11 +28,15 @@ class ProjectDetail extends Component {
     role: 3,
     newMilestone: '',
     projectMembers: [],
-    milestones: []
+    milestones: [],
+    roles: [],
+    roleMembers: [],
+    loading: true,
   };
 
   componentDidMount() {
     this.loadProject();
+    this.loadRoles();
   };
 
   loadProject = () => {
@@ -54,7 +66,66 @@ class ProjectDetail extends Component {
       .catch(err => console.log(err));
   };
 
-  //Handler for input change -- can be used for Role or Milestone
+  loadRoles = () => {
+    console.log(this.state.project.id)
+    API.getRoles(this.state.project.id)
+      .then(res => {
+        console.log("loadRoles");
+        console.log(res.data);
+        this.setState({
+          roles: res.data
+        })
+        if (this.state.loading) {
+          this.loadRoleMembers();
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
+  loadRoleMembers = () => {
+    console.log("loadRoleMembers");
+    API.getRoleMembers(this.state.project.id)
+      .then(res => {
+        console.log("Role Members:")
+        console.log(res.data);
+        let roleMembers = res.data;
+        const usersRoles = roleMembers.filter(role => role.userID == this.state.user.id);
+        console.log("User's Roles:");
+        console.log(usersRoles);
+        console.log("Roles:");
+        console.log(this.state.roles);
+        const roles = this.state.roles.map(role => {
+          if (usersRoles.length == 0 || null || typeof usersRole == undefined) {
+            role.usersStatus = "Not Applied";
+            return role;
+          }
+          for (let i = 0; i < usersRoles.length; i++) {
+            if (usersRoles[i].roleID == role.id) {
+              role.usersStatus = usersRoles[i].statusID;
+              role.usersRoleMemberId = usersRoles[i].id;
+              return role;
+            }
+            role.usersStatus = "Not Applied";
+            return role;
+          }
+        })
+        console.log("Altered Roles:");
+        console.log(roles);
+        console.log("Role Members:");
+        console.log(usersRoles);
+        roleMembers.sort((a, b) => (a.User.id > b.User.id) ? 1 : ((b.User.id > a.User.id) ? -1 : 0));
+        this.setState({
+          roles,
+          roleMembers,
+          loading: false
+        })
+        console.log("Roles");
+        console.log(roles);
+      })
+      .catch(err => console.log(err));
+  };
+
+  // Handler for input change that can be used for Role or Milestone
   handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
@@ -88,6 +159,65 @@ class ProjectDetail extends Component {
   deleteMilestone = (id) => {
     API.deleteMilestone(id)
     .then(res => this.loadMilestones())
+    .catch(err => console.log(err));
+  };
+
+  createRoleMember = (roleId) => {
+    console.log("createRoleMember");
+    const roleMember = {
+      roleID: roleId,
+      userID: this.state.user.id,
+      statusID: 6,
+      projectID: this.state.project.id
+    }
+    console.log(roleMember);
+    API.postRoleMember(roleMember)
+    .then(res => {
+      this.loadRoleMembers();
+    })
+    .catch(err => {
+      console.log("Error:");
+      console.log(err);
+    });
+  };
+
+  updateRoleMember = (roleMemberId, newStatus) => {
+    let roleMember = {
+      id: roleMemberId,
+      statusID: newStatus
+    }
+    API.updateRoleMember(roleMember)
+    .then(res => {
+      this.loadRoleMembers();
+    })
+    .catch(err => console.log(err));
+  }
+
+  deleteRoleMember = (roleMemberId) => {
+    API.deleteRoleMember(roleMemberId)
+    .then(res => {
+      this.loadRoleMembers();
+    })
+    .catch(err => console.log(err));
+  };
+
+  updateRole = (roleId, newStatus) => {
+    let role = {
+      id: roleId,
+      statusID: newStatus
+    }
+    API.updateRole(role)
+    .then(res => {
+      this.loadRoles();
+    })
+    .catch(err => console.log(err));
+  };
+
+  deleteRole = (roleId) => {
+    API.deleteRole(roleId)
+    .then(res => {
+      this.loadRoles();
+    })
     .catch(err => console.log(err));
   };
 
@@ -147,9 +277,58 @@ class ProjectDetail extends Component {
                   </Row>
                   <p className="text-center detail-text">{this.state.description}</p>
                   <Row>
+                    <Col size="md-12">
+                      <h1>Members</h1>
+                      {this.state.roleMembers.length ? (
+                        this.state.roleMembers.map(roleMember => (
+                        // (roleMember.statusID != 11 &&
+                          <MemberCard
+                            key={roleMember.ProjectRole.id}
+                            membersName={`${roleMember.User.firstName} ${roleMember.User.lastName}`}
+                            membersPosition={roleMember.User.position}
+                          >
+                            <MemberCardListGroup>
+                              <MemberCardListItem roleName={roleMember.ProjectRole.title}>
+                                <div className="btn-group btn-group-sm" role="group" aria-label="Role Member Options">
+                                  {(this.state.user.id == this.state.project.ownerId && roleMember.statusID == 6) &&
+                                    <>
+                                      <Button
+                                        className="btn btn-secondary"
+                                        onClick={() => this.updateRoleMember(roleMember.id, 7)}
+                                      >
+                                        Accept
+                                      </Button>
+                                      <Button
+                                        className="btn btn-secondary"
+                                        onClick={() => this.updateRoleMember(roleMember.id, 8)}
+                                      >
+                                        Decline
+                                      </Button>
+                                    </>
+                                  }
+                                  {((this.state.user.id == this.state.project.ownerId || this.state.user.id == roleMember.User.id) && roleMember.statusID == 7) &&
+                                    <Button
+                                      className="btn btn-secondary"
+                                      onClick={() => this.updateRoleMember(roleMember.id, 11)}
+                                    >
+                                      Retire
+                                    </Button>
+                                  }
+                                </div>
+                              </MemberCardListItem>
+                            </MemberCardListGroup>
+                          </MemberCard>
+                        // )
+                        ))
+                      ) : (
+                        <h3 className="none-listed">No Current Members</h3>
+                      )}
+                    </Col>
+                  </Row>
+                  <Row>
                     <Col size="lg-6 md-12">
                       <div className="detail-list-section">
-                        <h2>Team Roles</h2>
+                        <h2>Roles</h2>
                         <div className="add-project-card">
                           <Form >
                             <FormGroup >
@@ -165,33 +344,63 @@ class ProjectDetail extends Component {
                           </Form>
                         </div>
                         <div className="project-pg-list">
-                        <RolesAndMembers />
-                        {this.state.projectMembers.length ? (
-                          <List >
-                            <ProjectRoleEditModal />
-                            <RoleApplicantModal />
-                            {this.state.projectMembers.map(member => (
-                              <ListItem key={member.id}>
-                                <Card >
-                                  <h2>
-                                    {member.role}
-                                  </h2>
-                                  <RoleDropBtn />
-                                  <h3>
-                                    {member.User.firstName}
-                                  </h3>
-                                  <h4>
-                                    {/* currently user total */}
-                                    {member.User.hoursEarned}
-                                  </h4>
-                                  <h4>
-                                    {member.statusID}
-                                  </h4>
-                                </Card>
-                              </ListItem>
-                            ))}
-                          </List>
-                        ) : (
+                          {this.state.roles.length ? (
+                            this.state.roles.map(role => (
+                              <ColorCard key={role.id}>
+                                <ColorCardBody roleTitle={role.title} description={role.description}>
+                                  {role.statusID == 4 && <span className="badge badge-success">Open</span>}
+                                  {role.statusID == 5 && <span className="badge badge-danger">Closed</span>}
+                                </ColorCardBody>
+                                <ColorCardFooter>
+                                  {role.usersStatus == 6 && <span className="badge badge-primary">Applied</span>}
+                                  {role.usersStatus == 7 && <span className="badge badge-success">Approved</span>}
+                                  {role.usersStatus == 8 && <span className="badge badge-danger">Declined</span>}
+                                  <div className="btn-group btn-group-sm" role="group" aria-label="Role Options">
+                                    {(this.state.user.id == this.state.project.ownerId && role.statusID == 4) &&
+                                      <>
+                                        <Button
+                                          className="btn btn-secondary"
+                                          onClick={() => this.updateRole(role.id, 5)}
+                                        >
+                                          Close
+                                        </Button>
+                                        <Button
+                                          className="btn btn-secondary"
+                                          onClick={() => this.deleteRole(role.id)}
+                                        >
+                                          Delete
+                                        </Button>
+                                      </>
+                                    }
+                                    {(this.state.user.id == this.state.project.ownerId && role.statusID == 5) &&
+                                      <Button
+                                        className="btn btn-secondary"
+                                        onClick={() => this.updateRole(role.id, 4)}
+                                      >
+                                        Open
+                                      </Button>
+                                    }
+                                    {(this.state.user.id != this.state.project.ownerId && role.usersStatus == "Not Applied") &&
+                                      <Button
+                                        className="btn btn-secondary"
+                                        onClick={() => this.createRoleMember(role.id)}
+                                      >
+                                        Apply
+                                      </Button>
+                                    }
+                                    {(this.state.user.id != this.state.project.ownerId && role.usersStatus == 6) &&
+                                      <Button
+                                        className="btn btn-secondary"
+                                        onClick={() => this.deleteRoleMember(role.usersRoleMemberId)}
+                                      >
+                                        Cancel
+                                      </Button>
+                                    }
+                                  </div>
+                                </ColorCardFooter>
+                              </ColorCard>
+                            ))
+                          ) : (
                             <h3 className="none-listed">No Roles Created</h3>
                           )}
                           </div>
@@ -216,7 +425,9 @@ class ProjectDetail extends Component {
                               className="btn blue-btn card-item-submit"
                               type="submit"
                               onClick={this.submitMilestone}
-                            >Submit</FormBtn>
+                            >
+                              Submit
+                            </FormBtn>
                           </Form>
                         </div>
                         <div className="project-pg-list">
