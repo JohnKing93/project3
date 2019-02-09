c6a289f38655a2efda9506d3af1197f2a63ebdde
chuman <42072991+chrishuman0923@users.noreply.github.com>
2019-02-09 13:03:25 -0500

    Add env variables for production database

--- server/config/config.js
+++ server/config/config.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'root',
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_LOCAL_PASSWORD,
     database: 'progro',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
 };
